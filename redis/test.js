0196ff2b3adb5b351b5607914871bbe34f98ef9d
zhongyu <zhongyu@fancyguo.cn>
2013-05-09 17:46:27 +0800

    redis social graph

--- redis/test.js
+++ redis/test.js
@@ -21,7 +21,47 @@ function createUsers(users, fn) {
   }
 }
 
+function hydrate (users, fn) {
+  var total = Object.keys(users).length;
+  for (var i in users) {
+    (function (email, data) {
+      User.find(email, function (err, user) {
+        if (err) throw err;
+        users[email] = user;
+        --total || fn();
+      });
+    })(i, users[i]);
+  }
+}
+
+/*Too much */
 createUsers(testUsers, function () {
   console.log(Object.keys(testUsers).length + " users created");
-  process.exit();
+  hydrate(testUsers, function () {
+    testUsers['hello@app.com'].follow('hi@app.com', function (err) {
+      if (err) throw err;
+      console.log('hello followed hi');
+      
+      testUsers['hi@app.com'].getFollowers(function (err, users) {
+        if (err) throw err;
+        console.log("hi's fans: ", users);
+
+        testUsers['hi@app.com'].getFriends(function (err, users) {
+          if (err) throw err;
+          console.log("hi's friends: ", users);
+
+          testUsers['hi@app.com'].follow('hello@app.com', function (err) {
+            if (err) throw err;
+            console.log('hi followed hello too');
+
+            testUsers['hi@app.com'].getFriends(function (err, users) {
+              if (err) throw err;
+              console.log("hi's friends:", users);
+              process.exit(0);
+            });
+          });
+        });
+      });
+    });
+  });
 });
